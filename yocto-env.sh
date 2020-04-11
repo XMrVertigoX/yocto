59785139612be7f0e76e15391f80c2df9aa53e3e
Caspar Friedrich <c.s.w.friedrich@gmail.com>
2020-04-11 14:00:59 +0200

    tmp

--- yocto-env.sh
+++ yocto-env.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# --mount=type=volume,src=downloads,dst=/var/tmp/yocto/downloads,volume-driver=local,volume-opt=type=nfs4,volume-opt=device=:/srv/yocto/downloads,volume-opt=o=addr=192.168.178.123
+# --mount=type=volume,src=sstate-cache,dst=/var/tmp/yocto/sstate-cache,volume-driver=local,volume-opt=type=nfs4,volume-opt=device=:/srv/yocto/sstate-cache,volume-opt=o=addr=192.168.178.123
+
 docker run \
        --interactive \
        --mount=type=bind,source=$(pwd),target=/workdir \
-       --mount=type=volume,src=downloads,dst=/var/tmp/yocto/downloads,volume-driver=local,volume-opt=type=nfs4,volume-opt=device=:/srv/yocto/downloads,volume-opt=o=addr=192.168.178.123 \
-       --mount=type=volume,src=sstate-cache,dst=/var/tmp/yocto/sstate-cache,volume-driver=local,volume-opt=type=nfs4,volume-opt=device=:/srv/yocto/sstate-cache,volume-opt=o=addr=192.168.178.123 \
+       --mount=type=bind,source=/var/tmp/yocto/downloads,target=/var/tmp/yocto/downloads \
+       --mount=type=bind,source=/var/tmp/yocto/sstate-cache,target=/var/tmp/yocto/sstate-cache \
        --rm \
        --tty \
        --workdir=/workdir \
